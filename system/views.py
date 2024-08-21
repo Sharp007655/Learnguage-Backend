f4e20deb72fcfe3b8b88aae4f303289a0c4de0b3
Soma Ando <soma.ando.1227@gmail.com>
2024-08-21 13:12:50 +0900

    Recieve Message by Webhook

--- system/views.py
+++ system/views.py
@@ -1,6 +1,18 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .constant import *
+from .models import *
+from . import endpoint
+import json
 
 # Create your views here.
-def webhook(response):
+@csrf_exempt
+def webhook(request):
+    
+    data = json.loads(request.body.decode('utf-8'))
+    
+    if request.method == POST:
+        
+        print(data['events'][0]['source']['userId'], data['events'][0]['message']['text'])
     
     return JsonResponse({ 'message': 'webhook' })
